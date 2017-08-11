bfe6f880027e428437d3ebe488cbf3cb42bbd237
Nathan Stewart <stewart.nathang@gmail.com>
2017-08-11 15:28:58 -0700

    add delete individual answer functionality

--- app/routes/question.js
+++ app/routes/question.js
@@ -28,6 +28,11 @@ export default Ember.Route.extend({
     destroyQuestion(question) {
       question.destroyRecord();
       this.transitionTo('index');
+    },
+
+    destroyAnswer(answer) {
+      answer.destroyRecord();
+      this.transitionTo('question');
     }
   }
 });
