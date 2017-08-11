771d75b418ac78fa38f516169b341dcfbeb3c2a7
Nathan Stewart <stewart.nathang@gmail.com>
2017-08-11 10:35:26 -0700

    add firebase to app, config env, update model hook for firebase

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,19 +1,7 @@
 import Ember from 'ember';
 
-var questions = [{
-  id: 1,
-  author: "Joe Schmo",
-  content: "How do I make a textbox in html?",
-  notes: "I tried this: <input value='textbox'>"
-}, {
-  id: 2,
-  author: "Bob Dobbs",
-  content: "How do I start an Ember server?",
-  notes: "I tried: $ ember -g server"
-}];
-
 export default Ember.Route.extend({
   model() {
-     return questions;
+     return this.store.findAll('question');
    },
 });
--- config/environment.js
+++ config/environment.js
@@ -18,6 +18,13 @@ module.exports = function(environment) {
       }
     },
 
+    firebase: {
+      apiKey: "AIzaSyCiU5Ki-UCQzIBSb0tiXF7PazXT8PqG0PU",
+    authDomain: "techama-d05fd.firebaseapp.com",
+    databaseURL: "https://techama-d05fd.firebaseio.com",
+    storageBucket: "techama-d05fd.appspot.com"
+    },
+
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
