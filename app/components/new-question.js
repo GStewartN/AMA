4b5f997685e512cee2835109b856d270785e9e25
Nathan Stewart <stewart.nathang@gmail.com>
2017-08-11 11:26:24 -0700

    add new-question component, add functionality to enter and save a new question

---
+++ app/components/new-question.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+    questionFormShow() {
+      this.set('addNewQuestion', true);
+    },
+
+    saveQuestion() {
+      var params = {
+        author: this.get('author') ? this.get('author') : "Anonymous",
+        content: this.get('content') ? this.get('content') : "No Content...",
+        notes: this.get('notes') ? this.get('notes') : "No Notes..."
+      };
+      this.set('addNewQuestion', false);
+      this.sendAction('saveQuestion', params);
+    }
+  }
+});
